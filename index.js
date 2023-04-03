aa084f3db52cb17e0816c93b8eefe12d2d81f275
UrielCuriel <urielcuriel@outlook.com>
2023-04-03 13:18:41 -0600

    get pwd

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ const indexer = async () => {
     const algoliaAdminApiKey = core.getInput("algolia_admin_api_key");
     const algoliaIndexName = core.getInput("algolia_index_name");
     const collection_path = core.getInput("collection_path");
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.info(`The event payload: ${payload}`);
     const basePath = process.env.GITHUB_WORKSPACE;
     const client = algoliasearch(algoliaAppId, algoliaAdminApiKey);
     const index = client.initIndex(algoliaIndexName);
 
+    const pwd = process.cwd();
+    core.info(`pwd: ${pwd}`);
     const files = getFiles(collection_path, basePath);
     const records = files.map((file) => getHeaderContent(file, basePath));
     await index.saveObjects(records);
